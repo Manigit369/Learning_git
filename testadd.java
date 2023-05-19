c9a56ec8af6ea4969c87a0c94281b091952afa11
Mani <manikandan.gurug@gmail.com>
2023-05-19 13:33:26 +0530

    Committing a = 300

--- testadd.java
+++ testadd.java
@@ -3,14 +3,14 @@ public class testadd {
     public static void main(String[] args) {
 
 
-        int a = 400;
+        int a = 500;
         int b = 30;
         int c = a+b;
 
         System.out.println(c);
         
         
-        int d = 60;
+        int d = 70;
         int e = 50;
     }
 }
