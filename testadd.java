e67ea5a30cf49e5a540a020fe848b007bd7375c4
Manigit369 <133852440+Manigit369@users.noreply.github.com>
2023-05-19 13:34:34 +0530

    changing values

--- testadd.java
+++ testadd.java
@@ -3,14 +3,14 @@ public class testadd {
     public static void main(String[] args) {
 
 
-        int a = 400;
-        int b = 30;
+        int a = 700;
+        int b = 40;
         int c = a+b;
 
         System.out.println(c);
         
         
-        int d = 60;
-        int e = 50;
+        int d = 70;
+        int e = 60;
     }
 }
