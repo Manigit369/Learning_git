332a5be7fc3b2a1a68fedd927e99123f53b2e47c
Manigit369 <133852440+Manigit369@users.noreply.github.com>
2023-05-19 13:21:04 +0530

    b=30

--- testadd.java
+++ testadd.java
@@ -3,7 +3,7 @@ public class testadd {
     public static void main(String[] args) {
 
         int a = 100;
-        int b = 20;
+        int b = 30;
         int c = a+b;
 
         System.out.println(c);
